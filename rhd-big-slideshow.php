62ad4491eb9cdb6bff3ec4eb4f104e32b6441ebc
Nick Gaswirth <nick@roundhouse-designs.com>
2016-03-28 12:26:50 -0400

    Plugin details added

---
+++ rhd-big-slideshow.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: RHD Big Slideshow
+ * Description: Big full-screen custom slider, using cycle2
+ * Author: Roundhouse Designs
+ * Author URI: http://roundhouse-designs.com
+ * Version: 0.1
+**/
+
